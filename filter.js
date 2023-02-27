a87da59887bacee80334ec78b3f3fd34264f8d8b
Nikita <nikita.olechko@gmail.com>
2023-02-27 09:32:45 -0800

    Update filtering CSS to be in-line with document

--- filter.js
+++ filter.js
@@ -1,8 +1,10 @@
-document.getElementById("filter-button").addEventListener("click", function () {
-    var popup = document.getElementById("filter-popup");
-    if (popup.style.display === "none") {
-        popup.style.display = "block";
-    } else {
-        popup.style.display = "none";
-    }
+document.addEventListener("DOMContentLoaded", function () {
+    document.getElementById("filter-button").addEventListener("click", function () {
+        var popup = document.getElementById("filter-popup");
+        if (popup.style.display === "none") {
+            popup.style.display = "block";
+        } else {
+            popup.style.display = "none";
+        }
+    });
 });
