6ee53c0fc422b9e66ad10bf8070d7f765cad4800
Diane Choi <ekska21@gmail.com>
2023-03-09 19:15:08 -0800

    activate a tag in displayCardsDynamically function

--- scripts/main.js
+++ scripts/main.js
@@ -70,12 +70,14 @@ function displayCardsDynamically(collection) {
         var title = doc.data().name; // get value of the "name" key
         var details = doc.data().details; // get value of the "details" key
         var charityCode = doc.data().code; //get unique ID to each charity to be used for fetching right image
+        var docID = doc.id;
         let newcard = cardTemplate.content.cloneNode(true);
 
         //update title and text and image
         newcard.querySelector(".card-title").innerHTML = title;
         newcard.querySelector(".card-text").innerHTML = details;
         newcard.querySelector(".card-image").src = `./images/${charityCode}.jpg`; //Example: NV01.jpg
+        newcard.querySelector("a").href = "eachHike.html?docID=" + docID;
 
         //Optional: give unique ids to all elements for future use
         // newcard.querySelector('.card-title').setAttribute("id", "ctitle" + i);
