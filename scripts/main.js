65ceca2b60a63749cf447918209293111cf5ba9f
Nikita Olechko <nikita.olechko@gmail.com>
2023-03-10 16:08:31 -0800

    Implement database of weather events

--- scripts/main.js
+++ scripts/main.js
@@ -25,8 +25,7 @@ function writeCharities() {
     name: "Food Banks Canada",
     city: "Burnaby",
     province: "BC",
-    details:
-      "Our mission is to relieve hunger today and prevent hunger tomorrow.",
+    details: "Our mission is to relieve hunger today and prevent hunger tomorrow.",
     lat: 49.2467097082573,
     lng: -122.9187029619698,
     last_updated: firebase.firestore.FieldValue.serverTimestamp(), //current system time
@@ -36,8 +35,7 @@ function writeCharities() {
     name: "Inn from the Cold",
     city: "Anmore",
     province: "BC",
-    details:
-      "Inn from the Cold meets the needs of people who are homeless or at risk of becoming homeless. We work with the community and collaborate with our partners in supporting those with basic to complex needs in York Region.",
+    details: "Inn from the Cold meets the needs of people who are homeless or at risk of becoming homeless. We work with the community and collaborate with our partners in supporting those with basic to complex needs in York Region.",
     lat: 49.3399431028579,
     lng: -122.85908496766939,
     last_updated: firebase.firestore.FieldValue.serverTimestamp(),
@@ -47,14 +45,50 @@ function writeCharities() {
     name: "Furniture Bank",
     city: "North Vancouver",
     province: "BC",
-    details:
-      "Furniture Bank aim to ensure good furniture and housewares end up back into the community to support getting children off the floor, and families established in a furnished home.",
+    details: "Furniture Bank aim to ensure good furniture and housewares end up back into the community to support getting children off the floor, and families established in a furnished home.",
     lat: 49.38847101455571,
     lng: -122.94092543551031,
     last_updated: firebase.firestore.FieldValue.serverTimestamp(),
   });
 }
 
+function writeEvents() {
+  //define a variable for the collection you want to create in Firestore to populate data
+  var eventsRef = db.collection("events");
+
+  eventsRef.add({
+    code: "TR01",
+    name: "Turkey and Syria Earthquakes", //replace with your own city?
+    country: "Turkey",
+    details: "A series of devastating earthquakes have damaged or destroyed thousands of homes in Turkey and Syria, leaving thousands of people homeless. The earthquakes have also caused landslides and damaged roads, making it difficult for aid to reach those in need.",
+    date: "February 6th, 2023",
+    lat: 49.2467097082573,
+    lng: -122.9187029619698,
+    last_updated: firebase.firestore.FieldValue.serverTimestamp() //current system time
+  });
+  eventsRef.add({
+    code: "SP01",
+    name: "Spain Wildfires", //replace with your own city?
+    country: "Spain",
+    details: "Over 300,000 hectares have been scorched by wildfires in Spain, with the worst affected areas being in the south of the country. The fires have destroyed homes and forced thousands of people to evacuate.",
+    date: "December 17th, 2022",
+    lat: 49.2467097082573,
+    lng: -122.9187029619698,
+    last_updated: firebase.firestore.FieldValue.serverTimestamp() //current system time
+  });
+  eventsRef.add({
+    code: "UK01",
+    name: "War in Ukraine", //replace with your own city?
+    country: "Ukraine",
+    details: "On February 24th, 2022, Russia launched a military invasion of Ukraine. The invasion has resulted in the deaths of over 13,000 civilians and the displacement of over 8 million people.",
+    date: "February 24th, 2022",
+    lat: 49.2467097082573,
+    lng: -122.9187029619698,
+    last_updated: firebase.firestore.FieldValue.serverTimestamp() //current system time
+  });
+}
+
+writeEvents();
 //------------------------------------------------------------------------------
 // Input parameter is a string representing the collection we are reading from
 //------------------------------------------------------------------------------
