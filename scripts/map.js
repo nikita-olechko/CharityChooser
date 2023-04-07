f125fc194c196ba9a1ab06f28c725b024ab94ea5
Nikita Olechko <nikita.olechko@gmail.com>
2023-04-06 17:13:59 -0700

    Update map to be descriptive without buttons

--- scripts/map.js
+++ scripts/map.js
@@ -42,7 +42,7 @@ function showEventsOnMap() {
                         features.push({
                             'type': 'Feature',
                             'properties': {
-                                'description': `<strong>${event_name}</strong><p>${preview}</p> <br> <a href="/main.html?id=${doc.id}" class="pinnedEvent" target="" title="Opens in a new window"><button id="${doc.id}" class="button btn btn-success card-href event" name = "eventButton">Find Charities</button></a>`
+                                'description': `<strong>${event_name}</strong><p>${preview}</p> <br> <a href="/main.html?id=${doc.id}" class="pinnedEvent no-display" target="" title="Opens in a new window"><button id="${doc.id}" class="button btn btn-success card-href event" name = "eventButton">Find Charities</button></a>`
                             },
                             'geometry': {
                                 'type': 'Point',
