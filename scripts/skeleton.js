af3a9bb3a53f5bbee0b8a7c177aa0b0a73354102
Diane Choi <ekska21@gmail.com>
2023-04-06 18:19:23 -0700

    Remove redundant lines of code

--- scripts/skeleton.js
+++ scripts/skeleton.js
@@ -4,16 +4,8 @@
 //---------------------------------------------------
 function loadSkeleton() {
   firebase.auth().onAuthStateChanged(function (user) {
-    if (user) {
-      // User is signed in.
-      // Do something for the user here.
       console.log($("#navbarPlaceholder").load("./text/nav_after_login.html"));
       console.log($("#footerPlaceholder").load("./text/footer.html"));
-    } else {
-      // No user is signed in.
-      console.log($("#navbarPlaceholder").load("./text/nav_before_login.html"));
-      console.log($("#footerPlaceholder").load("./text/footer.html"));
-    }
   });
 }
 loadSkeleton(); //invoke the function
